cc9071f0b8f84adb006f8f47a76325b01592876f
Jean Vides <videsrudas@gmail.com>
2024-07-06 13:50:50 -0500

    shit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { Card, CardCredits } from "./(components)/components";
 
 export default function Home() {
     return (
@@ -32,35 +33,4 @@ export default function Home() {
             </div>
         </div>
     );
-}
-
-export function Card({ className, title, bg }: {
-    className?: string;
-    title: string;
-    bg: string;
-}) {
-    return (
-        <div className={`${className} rounded-lg bg-cover bg-center`} style={{ backgroundImage: `url(${bg})` }}>
-            <div className="w-full h-full m-0 p-0 md:p-4 lg:p-8 bg-black/40 hover:backdrop-blur-sm grid grid-cols-1 place-items-center place-content-center text-center rounded-lg break-normal gap-y-2">
-                <p className="text-xl md:text-2xl lg:text-4xl text-center text-[#f0f0f0] font-bold break-normal text-ellipsis">{title}</p>
-
-                <div className="py-2 px-12 rounded-lg bg-[#f0f0f0]/30 hover:bg-[#f0f0f0]/50 backdrop-blur-sm text-[#f7f7f7] hover:cursor-pointer">
-                    Read
-                </div>
-            </div>
-        </div>
-    )
-}
-
-
-export function CardCredits({ className }: {
-    className?: string;
-}) {
-    return (
-        <div className={`${className} rounded-lg bg-cover bg-center`}>
-            <div className="w-full h-full m-0 p-0 md:p-4 lg:p-8 hover:backdrop-blur-sm grid grid-cols-1 place-items-center place-content-center text-center rounded-lg break-normal gap-y-2 text-center text-[#0f0f0f]">
-                <p className="text-md md:text-md lg:text-lg font-semibold">Jean</p>
-            </div>
-        </div>
-    )
 }
